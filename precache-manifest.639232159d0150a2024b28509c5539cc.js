2f18d723e1d5d9b81f048d8514593e64060c9b3c
Administrator <yuy20170301@126.com>
2020-08-16 15:22:53 +0800

    deploy

---
+++ precache-manifest.639232159d0150a2024b28509c5539cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1bdb4b3f44337e424f",
+    "url": "/mormoney-vue/css/app.ebc07d3e.css"
+  },
+  {
+    "revision": "0e50ddb1c5b9ffe178abc067619e67ac",
+    "url": "/mormoney-vue/index.html"
+  },
+  {
+    "revision": "7a1bdb4b3f44337e424f",
+    "url": "/mormoney-vue/js/app.6a16fada.js"
+  },
+  {
+    "revision": "3358a666c2b0404fde23",
+    "url": "/mormoney-vue/js/chunk-vendors.1d2aff69.js"
+  },
+  {
+    "revision": "124e2d289a2fe5170ac3544f0e65b5ce",
+    "url": "/mormoney-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mormoney-vue/robots.txt"
+  }
+]);
